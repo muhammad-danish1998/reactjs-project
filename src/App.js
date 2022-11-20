6e8650a0c3a850afd03cabefea319ed86e85937f
muhammad-danish1998 <iamdanish1998@gmail.com>
2022-11-20 14:10:56 +0500

    git project add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
 
 function App() {
+  const [userList, setUserList] = useState([]);
+  const [ loading , setLoading] = useState(true)
+  const getUsers = async () => {
+    try {
+      let res = await fetch("https://api.github.com/users");
+      setLoading(false)
+      let data = await res.json();
+      console.log(data);
+      setUserList(data);
+    } catch (error) {
+      console.log("Error");
+    }
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+if(loading){
+ return <Loading />
+}
   return (
-    <div className="App">
-      <h1>Danish</h1>
-    </div>
+    <>
+      <div className="row">
+        {userList.map((val, ind) => {
+          const {login , avatar_url, type,id} = val
+          return (
+            <>
+              <div className="col" key = {ind}>
+                <div className="card" style={{ width: "18rem" }}>
+                  <img src={avatar_url} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">{login}</h5>
+                    <p className="card-text">
+                     {type}
+                    </p>
+                   
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
---
+++ src/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div style={{height:"100vh", display:"flex",alignItems:"center",justifyContent:"center"}}><h1>Loading ....</h1></div>
+  )
+}
+
+export default Loading
